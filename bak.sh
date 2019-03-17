b5bec0f4a684914f1374a07a96e9b89fe8cefaee
hx926453 <1115144916@qq.com>
2019-03-17 14:59:42 +0800

    提交hexo配置文件

---
+++ bak.sh
@@ -0,0 +1,24 @@
+#/bin/bash
+echo ============ push==> Coding ==============
+git remote add origin git@git.dev.tencent.com:hx926453/hx926453.git
+git add .
+
+echo ============ 提交配置文件 ==============
+git commit -m "提交hexo配置文件"
+echo ============ 新建分支并切换 ==============
+git branch hexo
+git checkout hexo
+echo ============ push==> Coding ==============
+git push origin hexo
+
+echo ============ push==> Github ==============
+git remote add origin git@github.com:hx926453/hx926453.github.io.git
+git add .
+
+echo ============ 提交配置文件 ==============
+git commit -m "提交hexo配置文件"
+echo ============ 新建分支并切换 ==============
+git branch hexo
+git checkout hexo
+echo ============ push==> Github ==============
+git push origin hexo
